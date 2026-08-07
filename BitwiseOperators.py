9a103d5bde12dfa3b49151696a8ec24ea340d633
harshithasri-ai <harshithasri5112@gmail.com>
2026-08-07 06:42:38 +0530

    BitwiseOperators.py

---
+++ BitwiseOperators.py
@@ -0,0 +1,13 @@
+# Bitwise Operators in Python
+
+print("\n===== Bitwise Operators =====")
+
+m = 10
+n = 4
+
+print("m & n =", m & n)
+print("m | n =", m | n)
+print("m ^ n =", m ^ n)
+print("~m =", ~m)
+print("m << 1 =", m << 1)
+print("m >> 1 =", m >> 1)
