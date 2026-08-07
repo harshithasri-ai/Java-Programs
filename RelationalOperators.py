77204dd700349c701520825d6bf27d6984b26034
harshithasri-ai <harshithasri5112@gmail.com>
2026-08-07 06:49:38 +0530

    RelationalOperators.py

---
+++ RelationalOperators.py
@@ -0,0 +1,13 @@
+# Relational (Comparison) Operators in Python
+
+print("\n===== Relational (Comparison) Operators =====")
+
+a = 10
+b = 20
+
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
