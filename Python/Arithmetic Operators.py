b994dea303612eac22df326707e0df25370e08d3
harshithasri-ai <harshithasri5112@gmail.com>
2026-08-21 08:03:15 +0530

    Arithmetic Operators.py

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,14 @@
+# Arithmetic Operators in Python
+
+a = 15
+b = 4
+
+print("===== Arithmetic Operators =====")
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
