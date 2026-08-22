7554bb4e0f58c808ccbc7ccf40edf22e73c75415
harshithasri-ai <harshithasri5112@gmail.com>
2026-08-22 06:34:02 +0530

    Membership Operator.py

---
+++ Python/Membership Operator.py
@@ -0,0 +1,9 @@
+# Membership Operators in Python
+
+print("\n===== Membership Operators =====")
+
+numbers = [10, 20, 30, 40]
+
+print("20 in numbers :", 20 in numbers)
+print("50 in numbers :", 50 in numbers)
+print("50 not in numbers :", 50 not in numbers)
