d0e09d4f5a5d134e71b6d26cf4e389bb937611c1
harshithasri-ai <harshithasri5112@gmail.com>
2026-08-22 06:54:15 +0530

    Delete Multiple2.py

--- Multiple2.py
+++
@@ -1,9 +0,0 @@
-# Program to Add and Multiply Complex Numbers
-
-c1 = complex(input("Enter first complex number (e.g., 2+3j): "))
-
-c2 = complex(input("Enter second complex number (e.g., 4+5j): "))
-
-print("Addition =", c1 + c2)
-
-print("Multiplication =", c1 * c2)
